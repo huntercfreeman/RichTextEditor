c5ecd5371ce4a8b3ad4ef8cd875fb4c616b1ae91
Hunter Freeman <huntercfreeman@gmail.com>
2022-07-03 14:33:59 -0400

    Tried to run app crashed

---
+++ RichTextEditor.RazorLib/RichTextEditorCase/TextTokenDisplay.razor.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using RichTextEditor.ClassLib.Store.RichTextEditorCase;
+
+namespace RichTextEditor.RazorLib.RichTextEditorCase;
+
+public partial class TextTokenDisplay : ComponentBase
+{
+    [Parameter]
+    public ITextToken TextToken { get; set; } = null!;
+}
