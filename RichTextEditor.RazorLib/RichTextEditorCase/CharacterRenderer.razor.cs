8d79e0eabe9f0077b01d8c51f84f610366dc7d49
Hunter Freeman <huntercfreeman@gmail.com>
2022-07-03 17:32:29 -0400

    Beginning sof cursor showing at index in plain text

---
+++ RichTextEditor.RazorLib/RichTextEditorCase/CharacterRenderer.razor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace RichTextEditor.RazorLib.RichTextEditorCase;
+
+public partial class CharacterRenderer : ComponentBase
+{
+    [Parameter]
+    // The html escaped character for space is &nbsp; which
+    // requires more than 1 character to represent therefore this is of type string
+    public string Character { get; set; } = null!;
+    [Parameter]
+    public bool ShouldDisplayCursor { get; set; }
+}
